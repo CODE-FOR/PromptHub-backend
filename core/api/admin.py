c7caabd464864057ce44bf781a6a0bac9426ec84
wucc <77946882+DanGuge@users.noreply.github.com>
2023-03-27 18:30:30 +0800

    Add response apis

--- core/api/admin.py
+++ core/api/admin.py
@@ -1,11 +1,13 @@
-from django.http import HttpRequest, HttpResponse
+from django.http import HttpRequest, JsonResponse
 from django.views.decorators.http import require_http_methods
 from core.models.user import User
+from .utils import response_wrapper, success_api_response, failed_api_responce
 
+@response_wrapper
 @require_http_methods("GET")
 def get_all_users(request: HttpRequest):
     users = User.objects.all()
     string = ""
     for user in users:
         string += user.nickname + "\n"
-    return HttpResponse(string)
+    return success_api_response({"name": string})
---
+++ core/api/utils.py
@@ -0,0 +1,51 @@
+"""
+utils for generating api responses
+"""
+import json
+from enum import Enum, unique
+
+from django.http import JsonResponse
+
+@unique
+class StatusCode(Enum):
+    SUCCESS = 200
+    BAD_REQUEST = 400
+    UNAUTHORIZED = 401
+    FORBIDDEN = 403
+    NOT_FOUND = 404
+    CONFLICT = 409
+    SERVER_ERROR = 501
+    ID_NOT_EXISTS = 555
+
+def api_responce(code: int, data: dict) -> dict:
+    return {
+        "code": code,
+        "data": data
+    }
+
+def failed_api_responce(status_code, error_msg=None) -> dict:
+    if error_msg is None:
+        error_msg = str(status_code)
+    else:
+        error_msg = str(status_code) + ': ' + error_msg
+
+    return api_responce(
+        code=status_code.value,
+        data={
+            "error_msg": error_msg
+        }
+    )
+
+def success_api_response(data: dict) -> dict:
+    return api_responce(
+        code=StatusCode.SUCCESS.value,
+        data=data
+    )
+
+def response_wrapper(func):
+    def inner(*args, **kwargs):
+        response = func(*args, **kwargs)
+        if isinstance(response, dict):
+            response = JsonResponse(response["data"])
+        return response
+    return inner
