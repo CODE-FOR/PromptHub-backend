e9efd011911376992ebe241c432709fa2f8958f7
xys <1422286969@qq.com>
2023-04-05 22:03:18 +0800

    fix github ci error

--- core/api/collections.py
+++ core/api/collections.py
@@ -1,13 +1,8 @@
-from django.http import HttpRequest, JsonResponse
+from django.http import HttpRequest
 from django.views.decorators.http import require_http_methods
-from django.utils import timezone
 
-from core.models.user import User, ConfirmCode
 
-from .auth import user_jwt_auth
-from .send_email import send_sign_in_email, send_forget_password_email
-from .utils import StatusCode, response_wrapper, success_api_response, failed_api_responce, \
-                   parse_data, failed_parse_data_response
+from .utils import response_wrapper, parse_data, failed_parse_data_response
 
 
 @response_wrapper
