290300a9e667791803ac313348324c845f73240d
wucc <77946882+DanGuge@users.noreply.github.com>
2023-04-06 23:10:21 +0800

    add methods in user & comment model

--- core/models/comment.py
+++ core/models/comment.py
@@ -19,3 +19,29 @@ class Comment(models.Model):
     parent_comment = models.ForeignKey("self", null=True, blank=True, on_delete=models.CASCADE)
     is_delete = models.BooleanField(default=False)
     
+    def simple_dict(self, **kwargs):
+        data = {
+            "id": self.id,
+            "user": self.user.simple_dict(),
+            "created_at": self.created_at,
+            "content": self.content,
+            "prompt_id": self.prompt.id
+        }
+
+        if self.is_delete:
+            data["content"] = "该评论已被删除"
+        else:
+            user_id = kwargs.get("user_id", None)
+            if user_id:
+                data["belong_to"] = user_id == self.user.id
+        return data
+    
+    def full_dict(self, **kwargs):
+        data = self.simple_dict(**kwargs)
+        if self.parent_comment:
+            data["parent_comment"] = self.parent_comment.simple_dict()
+        return data
+    
+    def safe_delete(self):
+        self.is_delete = True
+        self.save()
--- core/models/user.py
+++ core/models/user.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+DEFAULT_AVATAR = "https://s1.ax1x.com/2023/03/29/ppcWN24.jpg"
+
 class User(models.Model):
     """
     Fields:
@@ -15,11 +17,22 @@ class User(models.Model):
     password = models.CharField(max_length=256)
     nickname = models.CharField(max_length=256)
     
-    avatar = models.URLField(default="https://s1.ax1x.com/2023/03/29/ppcWN24.jpg")
+    avatar = models.URLField(default=DEFAULT_AVATAR)
     followers = models.ManyToManyField("User")
     is_confirmed = models.BooleanField(default=False)
     is_banned = models.BooleanField(default=False)
     is_delete = models.BooleanField(default=False)
+
+    def simple_dict(self):
+        data = {
+            "id": self.id,
+            "nickname": self.nickname,
+            "avatar": self.avatar
+        }
+        if self.is_delete:
+            data["nickname"] = "Unknown"
+            data["avatar"] = DEFAULT_AVATAR
+        return data
 
     def check_password(self, password):
         return self.password == password
